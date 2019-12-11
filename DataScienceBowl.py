237836266beccc05670aaf880fa65aa9c25c9d39
jai2shan <muralisraj@gmail.com>
2019-12-11 20:12:51 +0530

    Initiation

---
+++ DataScienceBowl.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 11 20:03:09 2019
+
+@author: mural
+"""
+
+%reset -f
+import pandas as pd, numpy as np, os
+
+os.chdir(r'D:\Kaggle\DataScience bowl\data-science-bowl-2019')
